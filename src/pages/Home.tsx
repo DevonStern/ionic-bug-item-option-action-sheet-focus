f9d187f6b07326fc10e8aaa488cd899fde1fa5e3
Devon Stern <devon.stern@cleantelligent.com>
2022-04-22 11:38:23 -0600

    Further simplified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,20 @@
-import { IonActionSheet, IonButton, IonContent, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonPage } from '@ionic/react';
-import { useRef, useState, useEffect } from 'react';
+import { IonActionSheet, IonContent, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonPage } from '@ionic/react';
+import { useRef, useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
-  const [name, setName] = useState<string>('test')
   const [isActionSheetOpen, setIsActionSheetOpen] = useState<boolean>(false)
-  const [isRename, setIsRename] = useState<boolean>(false)
+  const inputRef = useRef<HTMLIonInputElement | null>(null)
 
-  const showRenameInput = () => {
-    console.log('rename input')
-    setIsRename(true)
+  const setFocus = () => {
+    console.log('focus')
+    inputRef.current?.setFocus()
   }
 
-  const showRenameInput_workaround = () => {
-    console.log('rename input workaround')
+  const setFocus_workaround = () => {
     setTimeout(() => {
-      setIsRename(true)
+      console.log('focus workaround')
+      inputRef.current?.setFocus()
     }, 500)
   }
 
@@ -24,14 +23,11 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonItemSliding>
           <IonItem>
-            {isRename ?
-              <>
-                <RenameInput name={name} setName={setName} />
-                <IonButton onClick={() => setIsRename(false)}>Done</IonButton>
-              </>
-              :
-              <IonLabel>{name}</IonLabel>
-            }
+            <IonInput
+              ref={inputRef}
+              placeholder="placeholder"
+              onIonBlur={() => console.log('blur')}
+            />
           </IonItem>
           <IonItemOptions side="start">
             <IonItemOption onClick={() => setIsActionSheetOpen(true)} >
@@ -43,8 +39,8 @@ const Home: React.FC = () => {
           isOpen={isActionSheetOpen}
           buttons={[
             {
-              text: 'Rename',
-              handler: showRenameInput
+              text: 'Set Focus',
+              handler: setFocus
             }
           ]}
           onDidDismiss={() => setIsActionSheetOpen(false)}
@@ -55,45 +51,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
-
-
-interface RenameInputProps {
-  name: string
-  setName: React.Dispatch<React.SetStateAction<string>>
-}
-
-const RenameInput: React.FC<RenameInputProps> = ({ name, setName }) => {
-  const inputRef = useRef<HTMLIonInputElement | null>(null)
-  useInputFocus(inputRef)
-
-  return (
-    <IonInput
-      ref={inputRef}
-      onIonBlur={() => console.log('blur')}
-      value={name}
-      onIonChange={e => setName(e.detail.value ?? '')}
-    />
-  )
-}
-
-
-
-const useInputFocus = (inputRef: React.MutableRefObject<HTMLIonInputElement | null>) => {
-  // Ensure the internal `input` element is initialized before trying to focus on it
-  useEffect(() => {
-    const internalInputChecker = setInterval(() => {
-      inputRef.current?.getInputElement()
-        .then((element) => {
-          if (element) {
-            console.log('focus set')
-            inputRef.current?.setFocus()
-            clearInterval(internalInputChecker)
-          }
-        })
-    }, 50)
-    return () => {
-      clearInterval(internalInputChecker)
-    }
-  }, [])
-}
